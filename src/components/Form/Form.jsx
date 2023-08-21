784fe1b771b53d6654ad2174ca088c8452e1e7e3
Vitalii Okhota <119165474+VitaliiOkhota@users.noreply.github.com>
2023-08-21 19:02:09 +0300

    4 commit

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react'
+import './Form.css'
+import {useTelegram} from '../../hooks/useTelegram'
+
+const Form = () => {
+    const [country, setCountry] = useState('')
+    const [city, setCity] = useState('')
+    const [subject, setSubject] = useState('physical')
+    const {tg} = useTelegram()
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Відправити дані'
+        })
+    }, [])
+
+    useEffect(() => {
+        if (!country || !city) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, city])
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeCity = (e) => {
+        setCity(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
+    return (
+        <div className={'form'}>
+            <h3>Ввведіть ваші дані</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Країна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Місто'}
+                value={city}
+                onChange={onChangeCity}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'legal'}>Юридична особа</option>
+                <option value={'physical'}>Фізична особа</option>
+            </select>
+        </div>
+    )
+}
+
+export default Form
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '../Button/Button'
 import {useTelegram} from '../../hooks/useTelegram'
+import './Header.css'
 
 const Header = () => {
     const {user, onClose} = useTelegram();
@@ -9,7 +10,7 @@ const Header = () => {
         <div className={'header'}>
             <Button onClick={onClose}>Закрити</Button>
             <span className={'username'}>
-                {user.username}
+                {user?.username}
             </span>
         </div>
     )
